807105a960fd67608d7bd95146aec53f95c73305
Yxssssmin <yasmintate02@gmail.com>
2024-05-14 23:38:56 +0200

    Migracion tabla reservas

---
+++ database/migrations/2024_05_14_202734_create_reservas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->string('nombre_reserva', 100);
+            $table->text('tipo_comida');
+            $table->string('ubicacion', 100);
+            $table->integer('numero_personas');
+            $table->string('numero_contacto', 20);
+            $table->string('codigo_reserva', 20)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
